092fec4634b12a15e121d1dde987e309d6004131
Lapikova09 <96071466+Lapikova09@users.noreply.github.com>
2022-09-21 18:47:47 +0300

    Add files via upload

---
+++ катерина.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//Дано целое число A.Проверить истинность высказывания : «Число A является положительным».
+	//int a;
+	//cin >> a;
+	//bool res = a > 0;
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	//Дано целое число A. Проверить истинность высказывания: «Число A является нечетным»
+	//int a;
+	//cin >> a;
+	//bool res = a % 2 != 0;
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	//Дано целое число A. Проверить истинность высказывания: «Число A является четным».
+	//int a;
+	//cin >> a;
+	//bool res = a % 2 == 0;
+	//cout << boolalpha << res << endl;
+	//system("pause");	//int a,b;
+	//cin >> a >> b;
+	//bool res = a > 2 && b <= 3;
+	//cout << boolalpha << res << endl;
+	//system("pause");	//int a, b;
+	//cin >> a >> b;
+	//bool res = a >= 0 || b < -2;
+	//cout << boolalpha << res << endl;
+	//system("pause");	//Даны три целых числа: A, B, C. Проверить истинность высказывания: «Справедливо двойное неравенство A < B < C».
+	//int a, b, c;
+	//cin >> a >> b >> c;
+	//bool res = a < b && b < c; 
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	//Даны три целых числа: A, B, C. Проверить истинность высказывания: «Число B находится между числами A и C»
+	//int a, b, c;
+	//cin >> a >> b >> c;
+	//bool res = (a < b && b < c) || (c < b&&b < a);
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	//Даны два целых числа: A, B. Проверить истинность высказывания:
+	//«Каждое из чисел A и B нечетное»
+	//int a, b;
+	//cin >> a >> b;
+	//bool res = a % 2 == 0 && b % 2 == 0;
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	//Даны два целых числа: A, B. Проверить истинность высказывания:
+	//«Хотя бы одно из чисел A и B нечетное».
+	//int a, b;
+	//cin >> a >> b;
+	//bool res = a % 2 != 0 || b % 2 != 0;
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	//Даны два целых числа: A, B. Проверить истинность высказывания: «Ровно одно из чисел A и B нечетное».
+	//int a, b;
+	//cin >> a >> b;
+	//bool res = (a % 2 == 0 || b % 2 != 0)||(b % 2 == 0 || a % 2 != 0);
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	// Даны два целых числа : A, B.Проверить истинность высказывания : «Числа A и B имеют одинаковую четность».
+	//int a, b;
+	//cin >> a >> b;
+	//bool res = (a % 2 == 0 || b % 2 == 0)&&(b % 2 != 0 || a % 2 != 0);
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	//Даны три целых числа : A, B, C.Проверить истинность высказывания : «Каждое из чисел A, B, C положительное».
+	//int a, b, c;
+	//cin >> a >> b >> c;
+	//bool res = a > 0 && b > 0 && c > 0;
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	//Даны три целых числа: A, B, C. Проверить истинность высказывания: «Хотя бы одно из чисел A, B, C положительное».
+    //int a, b, c;
+	//cin >> a >> b >> c;
+    //bool res = a > 0 || b > 0 || c > 0;
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	//Даны три целых числа: A, B, C. Проверить истинность высказывания: «Ровно одно из чисел A, B, C положительное».
+	//int a, b, c;
+	//cin >> a >> b >> c;
+	//bool res = (a < 0 && b < 0 && c > 0) || (a > 0 && b < 0 && c < 0) || (a < 0 && b > 0 && c < 0);
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	//Даны три целых числа : A, B, C.Проверить истинность высказывания : «Ровно два из чисел A, B, C являются положительными»
+	//int a, b, c;
+	//cin >> a >> b >> c;
+	//bool res = (a < 0 && b > 0 && c > 0) || (a > 0 && b > 0 && c < 0) || (a > 0 && b < 0 && c > 0);
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	// Дано целое положительное число. Проверить истинность высказывания: «Данное число является четным двузначным».
+	//int a;
+	//cin >> a;
+	//bool res = a %2== 0 && a - 10 >= 0 && a + 1 <= 100;
+	//cout << boolalpha << res << endl;
+	//system("pause");
+	//. Дано целое положительное число.Проверить истинность высказывания : «Данное число является нечетным трехзначным».
+	//int a;
+	//cin >> a;
+	//bool res = a % 2 != 0 && a - 1 >= 99 && a + 1 <= 1000;
+	//cout << boolalpha << res << endl;
+	//system("pause");
+    // Проверить истинность высказывания: «Среди трех данных целых чисел есть хотя бы одна пара совпадающих».   // int a, b, c;
+    //cin >> a >> b >> c;
+   // bool res = (a == b) || (a == c) || (b == c);
+   //cout << boolalpha << res << endl;
+   //system("pause");
+}
